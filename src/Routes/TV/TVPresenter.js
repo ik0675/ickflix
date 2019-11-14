0af4eeccd8f3c9d5e1b481e8810ff0f740f5535f
남궁익 <ik0675@naver.com>
2019-11-13 19:07:58 -0600

    Loader, Presenter set-up with Home Grid Screen done

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,1 +1,41 @@
-export default () => "TV Presenter";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+import Section from "../../Components/Section";
+import Loader from "Components/Loader";
+
+const Container = styled.div`
+  padding: 0px 10px;
+`;
+
+const TVPresenter = ({ topRated, popular, airing, error, loading }) =>
+  loading ? (
+    <Loader />
+  ) : (
+    <Container>
+      {topRated && topRated.length > 0 && (
+        <Section title="Top Rated Shows">
+          {topRated.map(show => show.name)}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="Popular Shows">
+          {popular.map(show => show.name)}
+        </Section>
+      )}
+      {airing && airing.length > 0 && (
+        <Section title="Airing Today">{airing.map(show => show.name)}</Section>
+      )}
+    </Container>
+  );
+
+TVPresenter.propTypes = {
+  topRated: PropTypes.array,
+  popular: PropTypes.array,
+  airing: PropTypes.array,
+  error: PropTypes.string,
+  loading: PropTypes.bool.isRequired
+};
+
+export default TVPresenter;
